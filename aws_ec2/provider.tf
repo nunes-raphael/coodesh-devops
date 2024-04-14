f2acccc726b40877dfd19c6f7359662a30ec16a9
Raphael Nunes <oliveira.raphael@outlook.com>
2024-04-14 11:42:42 -0300

    first commit

---
+++ aws_ec2/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIA47CRXPOC4M4SHH5H"
+    secret_key = "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"
+}
---
+++ aws_vpc/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIA47CRXPOCYYNC3HIQ"
+    secret_key = "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"
+}
