68b0dba98d13fc8233fe36963b22adf1c632929f
Raphael Oliveira <77412061+nunes-raphael@users.noreply.github.com>
2024-04-14 16:00:04 -0300

    Update provider.tf

--- aws_ec2/provider.tf
+++ aws_ec2/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
-    secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+    access_key = "AKIA47CRXPOCY6JI7S7Z"
+    secret_key = "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"
 }
